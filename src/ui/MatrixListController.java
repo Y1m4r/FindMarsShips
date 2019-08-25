3e0d6418d330c29708b87a524cd4fe74659230b9
Juan Diego Naranjo <JuanNaranjo1206@gmail.com>
2019-08-25 17:16:11 -0500

    fxml all functions update

---
+++ src/ui/MatrixListController.java
@@ -0,0 +1,15 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class MatrixListController {
+
+    @FXML
+    private AnchorPane anchorPane;
+    
+    @FXML
+    void initialize() {
+        
+    }
+}
--- src/ui/MenuController.java
+++ src/ui/MenuController.java
@@ -1,15 +1,63 @@
 package ui;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MenuController {
 
     @FXML
-    private AnchorPane anchorPane;
+    private Button makeMatrix;
 
     @FXML
+    private Button randomMatrix;
+
+    @FXML
+    private Button matrixList;
+    
+    @FXML
     void initialize() {
-        anchorPane.setStyle("-fx-background-image: background.jpg");
+        
     }
+
+    @FXML
+    void generateMatrixList(ActionEvent event) throws IOException {
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("matrixList.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		stage.setTitle("List of Matrix");
+		stage.setScene(scene);
+		stage.show();
+    }
+
+    @FXML
+    void generateRandomMatrix(ActionEvent event) throws IOException {
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("randomMatrix.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		stage.setTitle("Random Matrix");
+		stage.setScene(scene);
+		stage.show();
+    }
+
+    @FXML
+    void makeMatrix(ActionEvent event) throws IOException {
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("makeMatrix.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		stage.setTitle("Make your Matrix");
+		stage.setScene(scene);
+		stage.show();
+    }
+
 }
